fa1be9657d2364fefc559bc2fff739aeae8dfd5f
Brandon <sugb73@gmail.com>
2021-03-15 10:54:18 -0400

    commit tree 25dedcbc92eb87e2c1d676bead4775adb5a185f0

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,30 @@
 # Write your code here
+
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+
+
+class GetRequester
+
+    def initialize(url)
+        @url = url
+    end    
+    
+    def get_response_body #sends GET request to URL
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+
+    def parse_json # use get_response_body to send a request
+    # return Ruby Array or Hash converted fromt eh response
+        response = JSON.parse(self.get_response_body)
+        # response.collect do |response|
+        #     response["agency"]  
+        # end
+    
+    end
+end 
